6cce987da441144f4ce247a34e4428b43567b5a9
leetangle <adszzz11@gmail.com>
2025-03-10 01:05:29 +0900

    :sparkles: [main] / application initialize

---
+++ src/main/kotlin/com/leetangle/sideproject/cscenter/CsCenterApplication.kt
@@ -0,0 +1,11 @@
+package com.leetangle.sideproject.cscenter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CsCenterApplication
+
+fun main(args: Array<String>) {
+    runApplication<CsCenterApplication>(*args)
+}
---
+++ src/test/kotlin/com/leetangle/sideproject/cscenter/CsCenterApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.leetangle.sideproject.cscenter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CsCenterApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
