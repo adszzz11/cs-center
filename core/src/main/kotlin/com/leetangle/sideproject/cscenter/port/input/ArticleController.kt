f953f14aada281b5d8ab2f5d7e86e8586e8a1f0c
leetangle <adszzz11@gmail.com>
2025-03-17 22:29:53 +0900

    ✨ [feature-article-001] / [미션4] 조회 REST API 만들기

--- core/src/main/kotlin/com/leetangle/sideproject/cscenter/port/input/ArticleController.kt
+++ core/src/main/kotlin/com/leetangle/sideproject/cscenter/port/input/ArticleController.kt
@@ -2,6 +2,7 @@ package com.leetangle.sideproject.cscenter.port.input
 
 import com.leetangle.sideproject.cscenter.framework.input.adapter.ArticleQueryInputAdapter
 import com.leetangle.sideproject.cscenter.framework.input.adapter.dto.ArticleOutput
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
@@ -11,10 +12,10 @@ class ArticleController(
     private val outputPort: ArticleQueryInputAdapter,
 ) {
     @GetMapping("/article")
-    fun getArticleDesc(): List<ArticleOutput> = outputPort.getArticleDesc()
+    fun getArticleDesc(): ResponseEntity<List<ArticleOutput>> = ResponseEntity.ok(outputPort.getArticleDesc())
 
     @GetMapping("/article/{articleId}")
     fun getArticleDetail(
         @PathVariable articleId: Long,
-    ): ArticleOutput = outputPort.getArticleDetail(articleId)
+    ): ResponseEntity<ArticleOutput> = ResponseEntity.ok(outputPort.getArticleDetail(articleId))
 }
